4d1f2a756e898ee0f4b53b20dcda46d68fd34368
arnoldhoward <arnold.howard20@outlook.com>
2019-07-13 01:00:54 +0800

    complete

--- main.py
+++ main.py
@@ -1,71 +1,99 @@
-# import cv2
-import time
+import cv2
+from utils import doAction, getWindowPos, getRectangle, moveFromInitTo
+from QRModule.qrdetect import hasQR
 
-totalX = 5
-totalY = 10
+totalX = 3
+totalY = 3
 curX = 0
 curY = 0
-
 interest_points = []
 directionX = True
-###
-#
-# type
-#     0: init x
-#     1: init y
-#     2: step x+
-#     3: step x-
-#     4: step y+
-#     5: step y-
-###
-
-def hasQR(image):
-    return True
-
-
-def click_real_button(type):
-    return
-
-
-def doAction(type, sleep=0.5):
-
-    delay(sleep)
-    return
-
-
-def delay(t):
-    time.sleep(t)
-    return
-
+windowTitle = 'Motion Controller Programming Software'
 
 def savePos(x, y):
+    global interest_points
     interest_points.append((x, y))
     return
 
 
-doAction('X0')
-doAction('Y0')
-while curY < totalY:
-    print(curX, curY)
+def imageStitching(l, t, r, b):
+    global curX, curY, totalX, totalY, directionX
+    # getWindowPos('Chrome')
 
-    # get image
-    image = None
-    if hasQR(image):
-        savePos(curX, curY)
+    # Initial Position
+    doAction('X0')
+    doAction('Y0')
+    cap = cv2.VideoCapture(0)
 
-    if directionX is True:
-        if curX+1 < totalX:
-            doAction('X+')
-            curX += 1
+    cap.set(3, 640)
+    cap.set(4, 480)
+
+    curX = l
+    curY = t
+    while t <= curY < b and cap.isOpened():
+        print(curX, curY)
+
+        # get image
+        ret, image = cap.read()
+
+        cv2.imshow('Robot', image)
+        key = cv2.waitKey(1)
+        if key & 0xFF == ord('q'):
+            break
+
+
+
+def perform():
+    global curX, curY, totalX, totalY, directionX
+    getWindowPos(windowTitle)
+
+# Initial Position
+    doAction('X0')
+    doAction('Y0')
+
+# Capture Camera
+    cap = cv2.VideoCapture(0)
+
+    cap.set(3, 640)
+    cap.set(4, 480)
+
+    while curY < totalY and cap.isOpened():
+        print(curX, curY)
+
+        # get image
+        ret, image = cap.read()
+
+        cv2.imshow('Robot', image)
+        key = cv2.waitKey(1)
+        if key & 0xFF == ord('q'):
+            break
+
+        haveQRCode, image = hasQR(image)
+
+        if haveQRCode:
+            savePos(curX, curY)
+
+        if directionX is True:
+            if curX+1 < totalX:
+                doAction('X+')
+                curX += 1
+            else:
+                doAction('Y+')
+                curY += 1
+                directionX = not directionX
         else:
-            doAction('Y+')
-            curY += 1
-            directionX = not directionX
-    else:
-        if curX-1 >= 0:
-            doAction('X-')
-            curX -= 1
-        else:
-            doAction('Y+')
-            curY += 1
-            directionX = not directionX
+            if curX-1 >= 0:
+                doAction('X-')
+                curX -= 1
+            else:
+                doAction('Y+')
+                curY += 1
+                directionX = not directionX
+
+    l, t, r, b = getRectangle(interest_points)
+    imageStitching(l, t, r, b)
+
+
+if __name__ == '__main__':
+    perform()
+
---
+++ utils.py
@@ -0,0 +1,118 @@
+import time
+import win32gui
+import win32ui
+import win32con
+import pyautogui
+
+hWnd = 0
+windowPos = (0, 0)
+btnInitX = (307, 162)
+btnInitY = (307, 210)
+btnXP = (250, 162)
+btnXM = (192, 162)
+btnYP = (250, 210)
+btnYM = (192, 210)
+
+
+def getWindowPos(title_text):
+    global hWnd, windowPos
+    _hwnd = _get_windows_bytitle(title_text, False)
+    print(_hwnd)
+    l, t, r, b = win32gui.GetWindowRect(_hwnd[0])
+    hWnd = _hwnd[0]
+    windowPos = (l, t)
+    print("windowPos-{}".format(windowPos))
+    return
+
+
+def _get_windows_bytitle(title_text, exact=True):
+    def _window_callback(hwnd, all_windows):
+        title = win32gui.GetWindowText(hwnd)
+        all_windows.append((hwnd, title))
+    windows = []
+    win32gui.EnumWindows(_window_callback, windows)
+    if exact:
+        return [hwnd for hwnd, title in windows if title_text == title]
+    else:
+        return [hwnd for hwnd, title in windows if title_text in title]
+
+
+def click_real_button(type):
+    global windowPos
+    win32gui.SetForegroundWindow(hWnd)
+    x = 0
+    y = 0
+    if type == 'X0':
+        x = windowPos[0] + btnInitX[0]
+        y = windowPos[1] + btnInitX[1]
+    elif type == 'Y0':
+        x = windowPos[0] + btnInitY[0]
+        y = windowPos[1] + btnInitY[1]
+    elif type == 'X+':
+        x = windowPos[0] + btnXP[0]
+        y = windowPos[1] + btnXP[1]
+    elif type == 'X-':
+        x = windowPos[0] + btnXM[0]
+        y = windowPos[1] + btnXM[1]
+    elif type == 'Y+':
+        x = windowPos[0] + btnYP[0]
+        y = windowPos[1] + btnYP[1]
+    elif type == 'Y-':
+        x = windowPos[0] + btnYM[0]
+        y = windowPos[1] + btnYM[1]
+
+    print(x, y)
+    pyautogui.moveTo(x, y)
+    pyautogui.click(interval=0.15)
+
+    return
+
+
+###
+#
+# type
+#     X0: init x
+#     Y0: init y
+#     X+: step x+
+#     X-: step x-
+#     Y+: step y+
+#     Y-: step y-
+#
+###
+
+def doAction(type, sleep=0.5):
+    click_real_button(type)
+    delay(sleep)
+    return
+
+
+def delay(t):
+    time.sleep(t)
+    return
+
+
+def moveFromInitTo(targetX, targetY):
+    for x in range(targetX):
+        doAction('X+')
+
+    for y in range(targetY):
+        doAction('Y+')
+
+def getRectangle(points):
+    l = 10000
+    t = 10000
+    r = 0
+    b = 0
+    for point in points:
+        if point[0] < l:
+            l = point[0]
+        if point[0] > r:
+            r = point[0]
+        if point[1] < t:
+            t = point[1]
+        if point[1] > b:
+            t = point[1]
+
+    return l, t, r, b
+
+
